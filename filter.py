621c5d6996213bb0c89d1eaa03216ba35b8597ff
Spencer Kee <spencerkee7@gmail.com>
2016-03-29 12:33:27 -0400

    added alphabetizing of hosts file and removing comments (both untested)

--- filter.py
+++ filter.py
@@ -2,6 +2,36 @@
 import time
 import os
 print 'started'
+
+def firstBlockLineNumber(filename):
+	with open(filename) as f:
+		for i, l in enumerate(f):
+			if '0.0.0.0' in l:
+				return i
+
+def sortHosts():
+	beginingSeperator = firstBlockLineNumber('/etc/hosts')
+	hosts = []
+	beginningLines = []
+	with open('/etc/hosts') as f:
+		websites = []
+		index = 0
+		for line in f:
+			if index < beginingSeperator:
+				beginningLines.append(line)
+			else:
+				hosts.append(line)
+			index += 1
+	hosts = [x.replace('#', '') for x in hosts if x != '\n']
+	hosts.sort()
+	open('/etc/hosts', 'w').close()
+	with open('etc/hosts', 'w') as f:
+		for i in beginningLines:
+			f.write(i)
+		for i in hosts:
+			f.write(i)
+
+
 def extract_urls_then_write():
 	if os.stat("/home/archie/Arch-Filter/inputsites").st_size != 0:
 		with open('/home/archie/Arch-Filter/inputsites') as f:
@@ -18,10 +48,13 @@ def extract_urls_then_write():
 					myfile.write('\n0.0.0.0 www.' + url)
 				else:
 					duplicateUrls.append(url)
+		sortHosts()
 		open('/home/archie/Arch-Filter/inputsites', 'w').close()
 		with open("/home/archie/Arch-Filter/inputsites", "a") as myfile:
 			for url in duplicateUrls:
 				myfile.write('\n' + url)
+
+
 if __name__ == '__main__':
 	while True:
 		extract_urls_then_write()
