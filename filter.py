bb2bead5afa88c0db7edc9017aca428d4118002e
Spencer Kee <spencerkee7@gmail.com>
2016-03-29 01:57:40 -0400

    updated paths. function will only launch when inputsites is not empty.

--- filter.py
+++ filter.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python2.7
 import time
+import os
+print 'started'
 def extract_urls_then_write():
-	with open('inputsites') as f:
-		websites = []
-		for line in f:
-			websites.append(line)
-	websites = [x.strip('\n') for x in websites] 
-	existingWebsites = open('testhosts').read()
-	with open("testhosts", "a") as myfile:
-		for url in websites:
-			if '0.0.0.0 ' + url not in existingWebsites:
-				myfile.write('\n0.0.0.0 ' + url)
-				myfile.write('\n0.0.0.0 www.' + url)
-	open('inputsites', 'w').close()
+	if os.stat("/home/archie/Arch-Filter/inputsites").st_size != 0:
+		with open('/home/archie/Arch-Filter/inputsites') as f:
+			websites = []
+			for line in f:
+				websites.append(line)
+		websites = [x.strip('\n') for x in websites] 
+		existingWebsites = open('/etc/hosts').read()
+		duplicateUrls = []
+		with open("/etc/hosts", "a") as myfile:
+			for url in websites:
+				if '0.0.0.0 ' + url not in existingWebsites:
+					myfile.write('\n0.0.0.0 ' + url)
+					myfile.write('\n0.0.0.0 www.' + url)
+				else:
+					duplicateUrls.append(url)
+		open('/home/archie/Arch-Filter/inputsites', 'w').close()
+		with open("/home/archie/Arch-Filter/inputsites", "a") as myfile:
+			for url in duplicateUrls:
+				myfile.write('\n' + url)
 if __name__ == '__main__':
 	while True:
 		extract_urls_then_write()
