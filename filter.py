d27141fe99dbe06f77d19e2728c4d14e051730ce
Spencer Kee <spencerkee7@gmail.com>
2016-03-28 23:20:58 -0400

    corrected potential error if one website is the ending of another
    example: if cracked.com was in the hosts file, then checking if ked.com was present would return true

--- filter.py
+++ filter.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python2.7
-	
-with open('inputsites') as f:
-	websites = []
-	for line in f:
-		websites.append(line)
-websites = [x.strip('\n') for x in websites] 
 
-print websites
-existingWebsites = open('testhosts').read()
+def extract_urls_then_write():
+	with open('inputsites') as f:
+		websites = []
+		for line in f:
+			websites.append(line)
+	websites = [x.strip('\n') for x in websites] 
+	existingWebsites = open('testhosts').read()
+	with open("testhosts", "a") as myfile:
+		for url in websites:
+			if '0.0.0.0 ' + url not in existingWebsites:
+				myfile.write('\n0.0.0.0 ' + url)
+				myfile.write('\n0.0.0.0 www.' + url)
+	open('inputsites', 'w').close()
 
-with open("testhosts", "a") as myfile:
-	for url in websites:
-		if url not in existingWebsites:
-			myfile.write('\n0.0.0.0 ' + url)
-			myfile.write('\n0.0.0.0 www.' + url)
-
-open('inputsites', 'w').close()
+extract_urls_then_write()
