22d0d6e3088710d8ebf89751456eca007ae373b6
Spencer Kee <spencerkee7@gmail.com>
2016-03-29 12:55:09 -0400

    changed direct references to filenames to variables for easier customization

--- filter.py
+++ filter.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python2.7
 import time
 import os
-print 'started'
+
+# hostsLocation = '/etc/hosts'
+# inputLocation = '/home/archie/Arch-Filter/inputsites'
+hostsLocation = '/home/archie/Arch-Filter/testhosts'
+inputLocation = '/home/archie/Arch-Filter/inputsites'
 
 def firstBlockLineNumber(filename):
 	with open(filename) as f:
@@ -10,10 +14,10 @@ def firstBlockLineNumber(filename):
 				return i
 
 def sortHosts():
-	beginingSeperator = firstBlockLineNumber('/etc/hosts')
+	beginingSeperator = firstBlockLineNumber(hostsLocation)
 	hosts = []
 	beginningLines = []
-	with open('/etc/hosts') as f:
+	with open(hostsLocation) as f:
 		websites = []
 		index = 0
 		for line in f:
@@ -24,38 +28,38 @@ def sortHosts():
 			index += 1
 	hosts = [x.replace('#', '') for x in hosts if x != '\n']
 	hosts.sort()
-	open('/etc/hosts', 'w').close()
-	with open('etc/hosts', 'w') as f:
+	print 'beginninglines', beginningLines
+	print 'hosts', hosts
+	open(hostsLocation, 'w').close()
+	with open(hostsLocation, 'w') as f:
 		for i in beginningLines:
 			f.write(i)
 		for i in hosts:
 			f.write(i)
 
-
 def extract_urls_then_write():
-	if os.stat("/home/archie/Arch-Filter/inputsites").st_size != 0:
-		with open('/home/archie/Arch-Filter/inputsites') as f:
+	if os.stat(inputLocation).st_size != 0:
+		with open(inputLocation) as f:
 			websites = []
 			for line in f:
 				websites.append(line)
 		websites = [x.strip('\n') for x in websites if x != '\n'] 
-		existingWebsites = open('/etc/hosts').read()
+		existingWebsites = open(hostsLocation).read()
 		duplicateUrls = []
-		with open("/etc/hosts", "a") as myfile:
+		with open(hostsLocation, "a") as myfile:
 			for url in websites:
 				if '0.0.0.0 ' + url not in existingWebsites:
 					myfile.write('\n0.0.0.0 ' + url)
 					myfile.write('\n0.0.0.0 www.' + url)
 				else:
 					duplicateUrls.append(url)
-		sortHosts()
-		open('/home/archie/Arch-Filter/inputsites', 'w').close()
-		with open("/home/archie/Arch-Filter/inputsites", "a") as myfile:
+		open(inputLocation, 'w').close()
+		with open(inputLocation, "a") as myfile:
 			for url in duplicateUrls:
 				myfile.write('\n' + url)
-
+	sortHosts()
 
 if __name__ == '__main__':
 	while True:
 		extract_urls_then_write()
-		time.sleep(60)
+		time.sleep(20)
